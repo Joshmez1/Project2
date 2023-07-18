9b100d56a3d0ad6e30ccac9e36f0f894ebb1c30a
Joshmez <104162551+Joshmez1@users.noreply.github.com>
2023-07-18 11:28:45 +0000

    Added Conts to generate random ID's between 0 - 53 which is the number of charactures in the API and only generate up to 16 of these which will be my game board

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,14 @@
 const gotAPIBaseURL = "https://thronesapi.com/api/v2/Characters/";
 
 const loadGameOfThrones= async () => {
+  const randomIds = new Set();
+  while(randomIds.size < 16 ) {
+    const randomNumber = Math.floor(Math.random() * 53 );
+    randomIds.add(randomNumber)
+  }
+
+  console.log([...randomIds])
+
   const res = await fetch(gotAPIBaseURL  + "1");
   const gameofthrones = await res.json();
   console.log(gameofthrones);
